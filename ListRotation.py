624b0070a5485908c4b411c136b9fa08b430b11a
Em-Esh-Magar <emeshthapamagar@gmail.com>
2025-08-09 21:05:36 +0545

    Shifting the place of list by key

---
+++ ListRotation.py
@@ -0,0 +1,19 @@
+# Rotation of a array/list by a key(shift by it)
+
+def ListRotation(arr,key):
+    array = arr.copy()
+    
+    for i in range(0,len(arr)):
+        next = (i%len(arr))+key
+        print(next)
+        if next > len(arr)-1:
+            next = next%len(arr)
+            
+            array[next] = arr[i]
+        else:
+            array[next] = arr[i]            
+            
+    return array
+
+
+print(ListRotation([3,1,6,5,9],3))
